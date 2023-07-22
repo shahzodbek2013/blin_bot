e15ce895a3667e5b48fd7a9683f932ff265183f5
shahzodbek2013 <139839676+shahzodbek2013@users.noreply.github.com>
2023-07-22 09:04:48 +0500

    kbzhsbjk
    qosimjon bedrovich

---
+++ bot.py
@@ -0,0 +1,23 @@
+import logging
+import types
+
+from aiogram import Bot, Dispatcher, executor
+API_TOKEN = '6141553139:AAFwM5w7NEFv7sBvQ4y2dJ4vVvEkzu7JqMQ'
+
+
+logging.basicConfig(level=logging.INFO)
+
+
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+
+
+
+@dp.message_handler(commands=['start'])
+async def start_bot (message: types.message):
+    await message.answer("salom")
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp)
